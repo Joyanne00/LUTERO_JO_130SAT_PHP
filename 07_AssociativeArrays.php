42c7b27cc8c623c3790c9e5e0bcae4be6180726d
Joy Anne Lutero <joyannesoledadlutero@gmail.com>
2025-02-11 00:26:17 +0800

    0210_07_AssociativeArrays_lec

---
+++ 07_AssociativeArrays.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>Recommended Books</h1>
+    <?php
+    $books = [
+        ['name' => 'Do Androids Dream of Electric Sheep', 'author' => 'Philip K. Dick', 'purchaseUrl' => 'https://example.com'],
+        ['name' => 'Project Hail Mary', 'author' => 'Andy Weir', 'purchaseUrl' => 'https://example.com']
+    ];
+    ?>
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <li><a href="<?= $book['purchaseUrl']; ?>"><?= $book['name']; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
