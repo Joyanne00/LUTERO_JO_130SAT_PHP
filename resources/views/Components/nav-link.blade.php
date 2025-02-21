c0b95a9622e39b1b57279d0345b3c2e9f254b2a1
Joy Anne Lutero <joyannesoledadlutero@gmail.com>
2025-02-21 15:14:00 +0800

    0220_08_Introduction_to_Migrations

---
+++ resources/views/Components/nav-link.blade.php
@@ -0,0 +1,11 @@
+@props(['active' => false ])
+
+@php
+
+    
+@endphp
+
+<a href="/" class="{{  $active ?'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}}bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+     aria-current="{{ $active ?'page': 'false' }}"
+     {{$attributes}}
+     >{{$slot}}</a>
